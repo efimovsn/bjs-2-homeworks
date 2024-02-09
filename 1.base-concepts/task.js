71eacb1a57c56cb003214f4e1e3b05157956ef3a
Efimovsn <efimovsn@bk.ru>
2024-02-09 21:49:00 +0500

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
-"use strict"
-function solveEquation(a, b, c) {
+"use strict"  
+
+function solveEquation(a, b, c) {  
   let arr = [];
-  
+  let dis = b**2-4*a*c;
+  let x = -b/(2*a);
+  let x1 = (-b + Math.sqrt(dis)) / (2*a);
+  let x2 = (-b - Math.sqrt(dis)) / (2*a); 
+  if (dis < 0) {    
+    console.log ("Дискриминант равен: " + dis + ", меньше нуля. Корней нет.")
+  } else if (dis === 0) {
+    arr.push(x);
+    console.log ("Дискриминант равен нулю. Один корень: " + x)
+  }  else {
+    arr.push(x1);
+    arr.push(x2);
+    console.log ("Дискриминант: " + dis)
+  }  
+  console.log(arr);
   return arr;
 }
 
