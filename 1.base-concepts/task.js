6915e7cab4a683b4537c8762564d58486d23911c
Efimovsn <efimovsn@bk.ru>
2024-02-17 23:28:51 +0500

    Первое задание после доработки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,31 +1,23 @@
 "use strict"  
 function solveEquation(a, b, c) {  
   let arr = [];
-  let dis = b**2-4*a*c;
-  let x = -b/(2*a);
-  let x1 = (-b + Math.sqrt(dis)) / (2*a);
-  let x2 = (-b - Math.sqrt(dis)) / (2*a); 
-  if (dis < 0) {    
-    console.log ("Дискриминант равен: " + dis + ", меньше нуля. Корней нет.")
-  } else if (dis === 0) {
-    arr.push(x);
-    console.log ("Дискриминант равен нулю. Один корень: " + x)
-  }  else {
-    arr.push(x1);
-    arr.push(x2);
-    console.log ("Дискриминант: " + dis)
-  }  
-  console.log(arr);
+  let discriminant = b**2-4*a*c;
+  if (discriminant === 0) {
+    arr.push(-b/(2*a));
+  }
+  else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2*a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2*a));
+  } 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) { 
-  let P = ((percent / 100) / 12);
-  let S = (amount - contribution);
-  let n = countMonths;
-  let Платёж = S * (P + (P / (((1 + P)**n) - 1)));
-  let totalAmount = parseFloat ((Платёж * 12).toFixed(2)); 
-  console.log(totalAmount);  
+  let interestRate = ((percent / 100) / 12);
+  let loanBody = (amount - contribution);
+  let numberOfMonths = countMonths;
+  let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate)**numberOfMonths) - 1)));
+  let totalAmount = parseFloat ((monthlyPayment * numberOfMonths).toFixed(2)); 
   return totalAmount  
 }
 
