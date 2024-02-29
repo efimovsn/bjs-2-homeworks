66ac08047774207cce5026e747efb5659c4b1aff
Efimovsn <efimovsn@bk.ru>
2024-02-29 21:02:01 +0500

    Реализовано задание по объектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;  
 }
+
+let student1  = new Student("Василиса", "женский", 19);
+let student2  = new Student("Артём", "мужской", 25);
+let marks = [];
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks){
+    if(this.marks === undefined){
+        this.marks = marks;   
+    } else {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    const sum = mark.reduce((acc, mark) => acc + mark, 0);
+    const length = mark.length;
+    return sum / length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason; 
 }
+
