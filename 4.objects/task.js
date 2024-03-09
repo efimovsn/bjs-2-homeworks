66c89526835e91fb12520f84cb76cf27bd58c588
Efimovsn <efimovsn@bk.ru>
2024-03-09 23:19:47 +0300

    Исправлено четвертое решение

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,34 @@
 function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
-    this.age = age;  
+    this.age = age;
+    this.marks = [];  
 }
 
 let student1  = new Student("Василиса", "женский", 19);
 let student2  = new Student("Артём", "мужской", 25);
-let marks = [];
 
-Student.prototype.setSubject = function (subjectName) {
+Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function(...marks){
-    if(this.marks === undefined){
-        this.marks = marks;   
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks === undefined) {
+        return;
+      } else {
+        this.marks.push(...marksToAdd);
+    }
+}   
+
+Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
     } else {
-        this.marks = this.marks.concat(marks);
+        return this.marks.reduce( (acc, mark) => acc + mark, 0 ) / this.marks.length;
     }
 }
 
-Student.prototype.getAverage = function () {
-    const sum = mark.reduce((acc, mark) => acc + mark, 0);
-    const length = mark.length;
-    return sum / length;
-}
-
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function(reason) {
     delete this.subject;
     delete this.marks;
     this.excluded = reason; 
